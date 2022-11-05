de9930228161ce7372ac56ba13710404e3fad412
Pavel Yatsenko <106490150+Pavelittio@users.noreply.github.com>
2022-11-05 15:34:57 +0200

    hw-08_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+      })
+      .join("");
+  }
+  
+  const galleryRef = document.querySelector(".gallery");
+  const addGalleryMarkup = createGalleryMarkup(galleryItems);
+  galleryRef.insertAdjacentHTML("afterbegin", addGalleryMarkup);
+  
+  galleryRef.addEventListener("click", onImgClick);
+  
+  const ligthbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  
+  function onImgClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+      return;
+    }
+  }
+  
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
 
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const textarea = document.querySelector('textarea')
+
+const inputValues = {
+    email: '',
+    message: ''
+}
+
+form.addEventListener('submit', clearAll)
+input.addEventListener('input', typeEmail)
+form.addEventListener('input', throttle(localStorageRenew, 500))
+textarea.addEventListener('input', typeMessage)
+window.addEventListener('load', pageLoad)
+
+function typeEmail(event) {
+    inputValues.email = event.currentTarget.value;
+}
+
+function typeMessage(event) {
+    inputValues.message = event.currentTarget.value;
+}
+
+function localStorageRenew() {
+    localStorage.setItem("feedback-form-state", JSON.stringify(inputValues));
+}
+
+function pageLoad() {
+    const lastTyped = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (lastTyped !== null) {
+        input.value = lastTyped.email;
+        textarea.value = lastTyped.message;}   
+}
+
+function clearAll(event) {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    localStorage.removeItem("feedback-form-state");
+    event.currentTarget.reset();
+}
